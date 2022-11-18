fc4885f975f39ba548c01e8b54b536e944a9fd6b
Maciej Rymarz <59456825+mako321@users.noreply.github.com>
2022-11-18 16:36:10 +0100

    Add translations (#51)

---
+++ resources/lang/pl/permissions.php
@@ -0,0 +1,12 @@
+<?php
+
+use EscolaLms\Templates\Enums\TemplatesPermissionsEnum;
+
+return [
+    TemplatesPermissionsEnum::TEMPLATES_CREATE => 'Utwórz szablon',
+    TemplatesPermissionsEnum::TEMPLATES_DELETE => 'Usuń szablon',
+    TemplatesPermissionsEnum::TEMPLATES_UPDATE => 'Aktualizuj szablon',
+    TemplatesPermissionsEnum::TEMPLATES_LIST => 'Lista szablonów',
+    TemplatesPermissionsEnum::TEMPLATES_READ => 'Wyświetl szablon',
+    TemplatesPermissionsEnum::EVENTS_TRIGGER => 'Wyzwól event',
+];
